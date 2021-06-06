c5ca7f7e5a9cfb8ce9628b8a26aa8c84ff9fdabe
abhinav anshul <abhinavanshul4u@gmail.com>
2021-06-06 21:03:49 +0530

    adds Dashboard Main

--- components/Home.js
+++ components/Home.js
@@ -7,31 +7,152 @@ import {
   ScrollView,
   ImageBackground,
 } from "react-native";
-import Feather from "react-native-vector-icons/Feather";
-import { SafeAreaView } from "react-native-safe-area-context";
 import colors from "../assets/colors/colors";
+import Feather from "react-native-vector-icons/Feather";
+import Entypo from "react-native-vector-icons/Entypo";
+import activitiesData from "../assets/data/activitiesData";
+import discoverCategoriesData from "../assets/data/discoverCategoriesData";
+import learnMoreData from "../assets/data/learnMoreData";
+import discoverData from "../assets/data/discoverData";
+import { SafeAreaView } from "react-native-safe-area-context";
 import profile from "../assets/images/person.png";
+import { FlatList, TouchableOpacity } from "react-native-gesture-handler";
 
-function Home({ navigation }) {
+Feather.loadFont();
+Entypo.loadFont();
+
+const Home = ({ navigation }) => {
+  const renderDiscoverItem = ({ item }) => {
+    return (
+      <TouchableOpacity
+        onPress={() =>
+          navigation.navigate("Details", {
+            item: item,
+          })
+        }
+      >
+        <ImageBackground
+          source={item.image}
+          style={[
+            styles.discoverItem,
+            { marginLeft: item.id === "discover-1" ? 20 : 0 },
+          ]}
+          imageStyle={styles.discoverItemImage}
+        >
+          <Text style={styles.discoverItemTitle}>{item.title}</Text>
+          <View style={styles.discoverItemLocationWrapper}>
+            <Entypo name='location-pin' size={18} color={colors.white} />
+            <Text style={styles.discoverItemLocationText}>{item.location}</Text>
+          </View>
+        </ImageBackground>
+      </TouchableOpacity>
+    );
+  };
+
+  const renderActivityItem = ({ item }) => {
+    return (
+      <View
+        style={[
+          styles.activityItemWrapper,
+          {
+            marginLeft: item.id === "activities-1" ? 20 : 0,
+          },
+        ]}
+      >
+        <Image source={item.image} style={styles.activityItemImage} />
+        <Text style={styles.activityItemText}>{item.title}</Text>
+      </View>
+    );
+  };
+
+  const renderLearnMoreItem = ({ item }) => {
+    return (
+      <ImageBackground
+        source={item.image}
+        style={[
+          styles.learnMoreItem,
+          {
+            marginLeft: item.id === "learnMore-1" ? 20 : 0,
+          },
+        ]}
+        imageStyle={styles.learnMoreItemImage}
+      >
+        <Text style={styles.learnMoreItemText}>{item.title}</Text>
+      </ImageBackground>
+    );
+  };
+
   return (
     <View style={styles.container}>
       <ScrollView>
-        {/* HEADER */}
+        {/* Header */}
         <SafeAreaView>
           <View style={styles.menuWrapper}>
             <Feather
               name='menu'
               size={32}
               color={colors.black}
-              style={styles.menuIcons}
+              style={styles.menuIcon}
             />
-            <Image source={profile} />
+            <Image source={profile} style={styles.profileImage} />
           </View>
         </SafeAreaView>
+
+        {/* Discover */}
+        <View style={styles.discoverWrapper}>
+          <Text style={styles.discoverTitle}>Discover</Text>
+          <View style={styles.discoverCategoriesWrapper}>
+            <Text
+              style={[styles.discoverCategoryText, { color: colors.orange }]}
+            >
+              All
+            </Text>
+            <Text style={styles.discoverCategoryText}>Destinations</Text>
+            <Text style={styles.discoverCategoryText}>Cities</Text>
+            <Text style={styles.discoverCategoryText}>Experiences</Text>
+          </View>
+          <View style={styles.discoverItemsWrapper}>
+            <FlatList
+              data={discoverData}
+              renderItem={renderDiscoverItem}
+              keyExtractor={(item) => item.id}
+              horizontal
+              showsHorizontalScrollIndicator={false}
+            />
+          </View>
+        </View>
+
+        {/* Activities */}
+        <View style={styles.activitiesWrapper}>
+          <Text style={styles.activitiesTitle}>Activities</Text>
+          <View style={styles.activitiesItemsWrapper}>
+            <FlatList
+              data={activitiesData}
+              renderItem={renderActivityItem}
+              keyExtractor={(item) => item.id}
+              horizontal
+              showsHorizontalScrollIndicator={false}
+            />
+          </View>
+        </View>
+
+        {/* Learn More */}
+        <View style={styles.learnMoreWrapper}>
+          <Text style={styles.learnMoreTitle}>Learn More</Text>
+          <View style={styles.learnMoreItemsWrapper}>
+            <FlatList
+              data={learnMoreData}
+              renderItem={renderLearnMoreItem}
+              keyExtractor={(item) => item.id}
+              horizontal
+              showsHorizontalScrollIndicator={false}
+            />
+          </View>
+        </View>
       </ScrollView>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -155,4 +276,5 @@ const styles = StyleSheet.create({
     marginVertical: 20,
   },
 });
+
 export default Home;
